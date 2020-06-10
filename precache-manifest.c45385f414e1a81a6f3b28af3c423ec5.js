c98556b413ee6d6309e779b5e7a1c7d4830885b1
toddpeters <tpeters@shaw.ca>
2020-06-10 17:28:39 -0600

    Updates

---
+++ precache-manifest.c45385f414e1a81a6f3b28af3c423ec5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "062b088f534860995d917d6b5caea0ba",
+    "url": "/react-web-site-temp/index.html"
+  },
+  {
+    "revision": "24d7135251c2e0be0156",
+    "url": "/react-web-site-temp/static/css/main.4c06968d.chunk.css"
+  },
+  {
+    "revision": "cdeccba009afeda2cdc3",
+    "url": "/react-web-site-temp/static/js/2.36d75c49.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-web-site-temp/static/js/2.36d75c49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24d7135251c2e0be0156",
+    "url": "/react-web-site-temp/static/js/main.67f2a063.chunk.js"
+  },
+  {
+    "revision": "b3194832a469eb33bf66",
+    "url": "/react-web-site-temp/static/js/runtime-main.4e274cbc.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/react-web-site-temp/static/media/logo.ee7cd8ed.svg"
+  }
+]);
